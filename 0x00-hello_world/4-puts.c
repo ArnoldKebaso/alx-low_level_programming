e6fd29014cd0ccda5e60699bb8ffeefe342da757
ArnoldKebaso <arnoldkebaso8@gmail.com>
2023-02-18 00:23:45 -0800

    Task 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
+#include <stdio.h>
 /**
- * main - A program that prints a line using the puts function
- * Return: 0 if Success.
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
+
