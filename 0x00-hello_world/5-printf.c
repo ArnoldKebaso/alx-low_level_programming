3d9c0f07dab90fa90a818f249887a3f98669abc0
ArnoldKebaso <arnoldkebaso8@gmail.com>
2023-02-18 00:11:47 -0800

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,8 @@
-#include<stdio.h>
-# !/bin/bash
-  /**
-  * main - A program that prints a line using printf function
-  * Return: 0 (Success)
-  * /
+#include <stdio.h>
+/**
+ * Function: main - Prints out a line  to stdout.
+ * Return: 0 if success.
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
